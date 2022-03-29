09f8c03ee6e0178a9c1b3ff862054c1ea21fe7c7
L.A.C.A. Rieff <webmaster@fannst.nl>
2022-03-29 14:33:39 +0200

    a

--- src/index.ts
+++ src/index.ts
@@ -8,31 +8,29 @@ import { HexDecoderStream as _HexDecoderStream } from "./HexDecoderStream";
 import { Base64DecoderStream as _Base64DecoderStream } from "./Base64DecoderStream";
 import { DotEscapeDecodeStream as _DotEscapeDecodeStream } from "./DotEscapeDecodeStream";
 
-export namespace llibencoding {
-    export type LineEncoderStream = _LineEncoderStream;
-    export const LineEncoderStream = _LineEncoderStream;
+export type LineEncoderStream = _LineEncoderStream;
+export const LineEncoderStream = _LineEncoderStream;
 
-    export type Base64EncoderStream = _Base64EncoderStream;
-    export const Base64EncoderStream = _Base64EncoderStream;
+export type Base64EncoderStream = _Base64EncoderStream;
+export const Base64EncoderStream = _Base64EncoderStream;
 
-    export type HexEncoderStream = _HexEncoderStream;
-    export const HexEncoderStream = _HexEncoderStream;
+export type HexEncoderStream = _HexEncoderStream;
+export const HexEncoderStream = _HexEncoderStream;
 
-    export type DotEscapeEncodeStream = _DotEscapeEncodeStream;
-    export const DotEscapeEncodeStream = _DotEscapeEncodeStream;
+export type DotEscapeEncodeStream = _DotEscapeEncodeStream;
+export const DotEscapeEncodeStream = _DotEscapeEncodeStream;
 
-    export type QuotedPrintableEncoderStream = _QuotedPrintableEncoderStream;
-    export const QuotedPrintableEncoderStream = _QuotedPrintableEncoderStream;
+export type QuotedPrintableEncoderStream = _QuotedPrintableEncoderStream;
+export const QuotedPrintableEncoderStream = _QuotedPrintableEncoderStream;
 
-    export type LineDecoderStream = _LineDecoderStream;
-    export const LineDecoderStream =_LineDecoderStream;
+export type LineDecoderStream = _LineDecoderStream;
+export const LineDecoderStream =_LineDecoderStream;
 
-    export type HexDecoderStream = _HexDecoderStream;
-    export const HexDecoderStream = _HexDecoderStream;
+export type HexDecoderStream = _HexDecoderStream;
+export const HexDecoderStream = _HexDecoderStream;
 
-    export type Base64DecoderStream = _Base64DecoderStream;
-    export const Base64DecoderStream = _Base64DecoderStream;
-    
-    export type DotEscapeDecodeStream = _DotEscapeDecodeStream;
-    export const DotEscapeDecodeStream = _DotEscapeDecodeStream;
-}
+export type Base64DecoderStream = _Base64DecoderStream;
+export const Base64DecoderStream = _Base64DecoderStream;
+
+export type DotEscapeDecodeStream = _DotEscapeDecodeStream;
+export const DotEscapeDecodeStream = _DotEscapeDecodeStream;
